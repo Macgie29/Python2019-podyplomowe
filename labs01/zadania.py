444c4afa94fef3fc660f076c96be6840362f0d6c
Tomasz Dwojak <t.dwojak@amu.edu.pl>
2019-01-26 08:23:20 +0100

    Add labs01

---
+++ labs01/zadania.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+zad. 1
+Przejrzyj plik podstawy.py. Zawiera on kopię materiałów z części wykładowej.
+"""
+
+"""
+zad. 2
+Wyświetl na ekranie pole koła o promieniu 3. Jako przybliżenie PI przyjmij 3.14.
+"""
+
+"""
+zad. 3
+ * Stwórz dwie zmienne imie i nazwisko i przypisz do dowolne wartości (nie muszą
+być prawdziwe).
+ * wypisz obie zmienne na ekran.
+ * Połącznie obie zmienne spacją i wynik zapisz do zmiennej dane.
+ * Wyświetl jaki typ ma zmienna dane.
+"""
+
+"""
+zad. 4
+Przekonwertuj wartość b na int, a następnie oblicz średnią z a, b i c.
+"""
+a = 314
+b = "500"
+c = 4.5
+
+"""
+zad. 5
+Stwórz zmienną pole_kwadratu i przypisz do liczbę: pole kwadratu o boku 3.
+"""
+
+"""
+zad. 6
+Wczytaj z klawiatury 3 liczby, skonwertuj je na dowolny typ liczbowy, a
+wynik zapisz do zmiennej suma.
+"""
+
+"""
+zad. 7
+Wyświetl napis "Suma pdanych liczb to: " <suma>, gdzie <suma> to zmienna
+z poprzedniego zadania.
+"""
+
+"""
+zad. 8
+Przekonwertuj zmienna <suma> jako str i stwórz nowy napis jako połączenie
+"Suma pdanych liczb to: " i przekonwertowanej wartości. Wynik zapisz do
+zmienne out. Wyświetl zmienną out. Powinien wyjść taki sam wynik jak w z. 7
+"""
+
+
