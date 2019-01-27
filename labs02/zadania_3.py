bbae263dde2de0b18f212b4ca212b939bdb84bf6
Tomasz Dwojak <t.dwojak@amu.edu.pl>
2019-01-27 08:55:03 +0100

    Add labs 02

---
+++ labs02/zadania_3.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Zadania na rozgrzewkę.
+"""
+
+
+"""
+zad. 1
+ * Stwórz listę składającą się z dowolnych 100 elementów, np. może być
+ to listę kwadratów liczb.
+ * Sprawdź za pomocą funkcji len liczbę elementów tej listy.
+ * Usuń trzeci, element.
+ * Usuń przedostatni element.
+ * Wyświetl pierwsze 10 elementów.
+"""
+
+"""
+zad. 2
+Znajdz najmniejsz element w poniższej liście.
+"""
+l = [0, 6, 9, -10, -5, 9, 8, -6]
+
+"""
+zad. 3
+Wyświetl poniższy słownik, tak,, aby każda para klucz: wartość
+była w osobnej linii.
+"""
+s = {'Tomasz': [3, 4, 5, 4], 'Agata': [5, 5, 5, 4]}
+
+
+"""
+zad. 4
+Poniżej jest podana lista liczby. Stwórz słownik <counter>, którego kluczami
+będą wartości występujące w liście <liczby>, a wartościami ile dany element
+wystąpił w liście <liczby>.
+"""
+liczby = [3, 4, 3, 3, 4, 7, 9]
+
+"""
+zad. 5
+Poniższy słownik oceny dwóch osób. Stwórz nowy słownik z takimi samymi kluczami,
+ale wartościami tego słownika będą średnie oceń.
+"""
+s = {'Tomasz': [3, 4, 5, 4], 'Agata': [5, 5, 5, 4]}
+
+
+"""
+zad. 6
+Dla podanego poniże słownika S, stwórz nowy słownik, którego kluczami będą
+wartości słownika S, a wartościami: odpowiadające im klucze z S.
+"""
+S = {'Klucz1': "Wartosc1", 'Klucz2': "Wartosc2", 'Klucz3': "Wartosc3"}
+
+"""
+zad. 7
+Napisz kod, który wypisze na ekran elementy, które występnują w obu poniżej
+podanych funkcjach.
+"""
+l1 = [99, 8, 7, 55]
+l2 = [55, 111, 11, 99, 8]
+
+"""
+zad. 8
+Napisz kod, który znajdzie najdroższy produkt  w poniższym słowniku.
+"""
+zakupy =  {'telefon': 1000, 'ładowarka': 35, 'chleb': 4.30, 'kawa': 55, 'gramofon': 240}
+
+"""
+zad. 9
+Stwórz listę składającą się z wartości słownika zakupy.
+"""
+zakupy =  {'telefon': 1000, 'ładowarka': 35, 'chleb': 4.30, 'kawa': 55, 'gramofon': 240}
+
+"""
+zad. 10
+Wyświetl na ekranie poniższy wzór:
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+"""
---
+++ labs02/zadania_4.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+zad. 1
+Napisz funkcję, która zwróci sumę elementów w liście.
+"""
+
+"""
+zad. 2
+Napisz funkcje, która dla przekazanej listy, zwróci listę z niepowtarzającymi
+się wartościami.
+"""
+
+"""
+zad. 3
+Napisz funkcję, która dla przekazanej listy zwróci listę skłdajacąs się
+z elementów na parzystych indeksach.
+"""
+
+"""
+zad. 4
+Napisz funkcję, która sprawdzi, czy podany napis (String) jest palindromem,
+czyli wyrazem, który czytany od końca jest tym samym słowem, np. kajak.
+"""
+
+"""
+zad. 5
+Napisz generator, który będzie zwracał n kolejnych liczb ciągu fibonacciego,
+gdzie n to argument funkcji.
+"""
+
+"""
+zad. 6
+Zainstaluj bibliotekę ``weather-api`` (https://pypi.python.org/pypi/weather-api). Korzystając z niej:
+ * Wypisz informacje o aktualnej pogodzie.
+ * Napisz funkcję, która zamieni stopnie ``F`` na ``C``.
+ * Korzystając z prognozy, znajdź dzień, w którym będzie najzimniej. Wypisz nazwę tygodnia (w języku polskim) i temperaturę w C.
+"""
+
+"""
+zad. 7
+Katalog scores zawiera 64 pliki tekstowe, które posiadają informacje o wysokości miary ``BLEU`` na różnych etapach trenowania modelu. Nazwa każdego pliku na postać  ``model.iterXXXXXXX.npz.bleu``, gdzie ``XXXXXXX``, to liczba iteracji.Zaw>
+ * Wykorzystaj bibliotekę ``glob`` (https://docs.python.org/2/library/glob.html)
+ * Wyświetl tylko pełną nazwe pliku (wraz z ścieżką).
+"""
+
+
+
