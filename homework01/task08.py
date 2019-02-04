81a4aa137c6970d27f90ffb2b89520d4e36cf35b
macgie29 <macgie29@gmail.com>
2019-02-04 22:16:00 +0100

    Zadanie 8

--- homework01/task08.py
+++ homework01/task08.py
@@ -7,7 +7,16 @@ przez 3 lub 5 mniejszych niż n.
 """
 
 def sum_div35(n):
-    pass
+    numbers = list(range(n))
+    right = []
+    for i in numbers:
+        if i < n and (i % 3 == 0 or i % 5 == 0): ## nie bylem pewien tego fragmentu pytania mniejsze niz n, wiec dodalem to i < n. Jeżeli to nie jest potrzebne, to ten fragment można usunąć :)
+            right.append(i)
+    score = sum(right)
+    return score
+
+print(sum_div35(1000))
 
 input = 100
 output = 2318
+
