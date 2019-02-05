487bf7ffc1457886523e9492a3d2971e30387a69
macgie29 <macgie29@gmail.com>
2019-02-05 16:40:02 +0100

    Zadanie 5

--- homework01/task05.py
+++ homework01/task05.py
@@ -10,7 +10,9 @@ np. odległość pomiędzy punktami (0, 0, 0) i (3, 4, 0) jest równa 5.
 """
 
 def euclidean_distance(x, y):
-    pass
+    return (((y[0] - x[0]) ** 2) + ((y[1] - x[1]) ** 2) + ((y[2] - x[2]) ** 2)) ** (1/2)
 
 input  = [[2.3, 4.3, -7.5], [2.3, 8.5, -7.5]]
 output = 4.2
+
+print(euclidean_distance(input[0], input[1]))
